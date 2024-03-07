195ce085b0ef81d0b14d47e329a16a80ed9e5ed2
muhammadabidyasir139 <muhamamdabidyasir139@gmail.com>
2024-03-07 15:44:36 +0700

    membuat prosedure input

--- bubble.cpp
+++ bubble.cpp
@@ -9,10 +9,20 @@ int arr[20];
 int n;
 
 void input() {
+    while (true) {
+        cout << "masukan banyaknya elemen oada array = " ;
+        cin >> n;
+        if (n <= 20)
+            break;
+        else {
+            cout << "Array dapat mempunyai maksimal 20 elemen.";
+        }
+    }
 
 }
 
-int main() {
-    cout << "masukan angka"
-}; 
+
+// int main() {
+//     cout << "masukan angka";
+// }; 
 
