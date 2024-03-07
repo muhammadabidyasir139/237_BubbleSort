77eee3420998bc94a4fdbacc8053359476d8ec23
muhammadabidyasir139 <muhamamdabidyasir139@gmail.com>
2024-03-07 15:33:36 +0700

    membuat variable

---
+++ bubble.cpp
@@ -0,0 +1,18 @@
+// ketentuan 5 commit
+// membuat prosedure input, bubble short, output memanggil prosedure, membuat variable
+// nama repo 3nim_BubbleShort
+
+#include <iostream>
+using namespace std;
+
+int arr[20];
+int n;
+
+void input() {
+
+}
+
+int main() {
+    cout << "masukan angka"
+}; 
+
